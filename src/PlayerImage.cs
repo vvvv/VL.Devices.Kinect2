08686fc0791315d6149123b5ffa23c57ab296705
ravazquez <ravazquez@gmail.com>
2019-05-09 16:24:44 -0600

    Added InfraredImage and PlayerImage classes. WIP

---
+++ src/PlayerImage.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Kinect;
+using VL.Lib.Basics.Imaging;
+
+namespace VL.Devices.Kinect2
+{
+    // Exposes the kinect frame buffer directly
+    class PlayerImage : IImage
+    {
+        readonly BodyIndexFrame frame;
+
+        public PlayerImage(BodyIndexFrame frame)
+        {
+            this.frame = frame;
+            // TODO: Not sure what the format is exactly
+            Info = frame.FrameDescription.ToImageInfo(PixelFormat.R8);
+        }
+
+        public ImageInfo Info { get; }
+
+        public bool IsVolatile => true;
+
+        public IImageData GetData()
+        {
+            return new KinectBufferImageData(frame.FrameDescription, frame.LockImageBuffer());
+        }
+    }
+}
