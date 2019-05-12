af76c79114b5bda2202938438fcaf612ae7cbdea
ravazquez <ravazquez@gmail.com>
2019-05-11 20:52:51 -0600

    Cleaned up using statements

--- src/ReaderExtensions.cs
+++ src/ReaderExtensions.cs
@@ -4,9 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reactive.Linq;
 using System.Reactive.Subjects;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 using VL.Lib.Basics.Imaging;
 
 namespace VL.Devices.Kinect2
