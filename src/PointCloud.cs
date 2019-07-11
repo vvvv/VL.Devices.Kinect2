bb2d3b5e5b9a581c8088495f57c8700421c737d5
ravazquez <ravazquez@gmail.com>
2019-07-11 11:34:34 -0600

    Added initial draft of PointCloud class

---
+++ src/PointCloud.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Kinect;
+using SharpDX;
+using System;
+using VL.Lib.Collections;
+
+namespace VL.Devices.Kinect2
+{
+    public static class PointCloud
+    {
+        public static unsafe Spread<Vector3>GetPoints(DepthImage image, float scale, float zScale)
+        {
+            DepthFrame frame = image.frame;
+            var width = image.Info.Width;
+            var height = image.Info.Height;
+            SpreadBuilder<Vector3> sb = new SpreadBuilder<Vector3>((int) (width * height * scale));
+            using (var buffer = frame.LockImageBuffer())
+            {
+                ushort* ptr = (ushort*)buffer.UnderlyingBuffer.ToPointer();
+                int step = (int)(1 / scale);
+                for (int row = 0; row < image.Info.Height; row += step)
+                {
+                    for (int col = 0; col < image.Info.Width; col += step)
+                    {
+                        ushort z = *ptr;
+                        sb.Add(new Vector3(col * scale, row * scale, z * zScale));
+                        ptr += step;
+                    }
+                }
+                return sb.ToSpread();
+            }
+        }
+    }
+}
