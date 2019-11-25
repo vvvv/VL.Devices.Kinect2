b5aa9bc7f4f25e81ae0867ac7bab6e004eda9097
ravazquez <ravazquez@gmail.com>
2019-11-24 20:20:59 -0600

    Added initial draft of FaceHD node plus example patch. (Currently outputs 2d points in color space)

---
+++ src/FaceHD.cs
@@ -0,0 +1,127 @@
+﻿using Microsoft.Kinect;
+using Microsoft.Kinect.Face;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Subjects;
+using Xenko.Core.Mathematics;
+
+namespace VL.Devices.Kinect2
+{
+    public class FaceHD : IDisposable
+    {
+        private KinectSensor _sensor = null;
+
+        private BodyFrameSource _bodySource = null;
+
+        private BodyFrameReader _bodyReader = null;
+
+        private HighDefinitionFaceFrameSource _faceSource = null;
+
+        private HighDefinitionFaceFrameReader _faceReader = null;
+
+        private FaceAlignment _faceAlignment = null;
+
+        private FaceModel _faceModel = null;
+        public IObservable<List<Vector2>> Points { get { return points; } }
+
+        private Subject<List<Vector2>> points;
+
+        private List<Vector2> _points;
+
+        public FaceHD(KinectSensor sensor)
+        {
+            _sensor = sensor;
+            _points = new List<Vector2>();
+            points = new Subject<List<Vector2>>();
+
+            if (_sensor != null)
+            {
+                _bodySource = _sensor.BodyFrameSource;
+                _bodyReader = _bodySource.OpenReader();
+                _bodyReader.FrameArrived += BodyReader_FrameArrived;
+
+                _faceSource = new HighDefinitionFaceFrameSource(_sensor);
+
+                _faceReader = _faceSource.OpenReader();
+                _faceReader.FrameArrived += FaceReader_FrameArrived;
+
+                _faceModel = new FaceModel();
+                _faceAlignment = new FaceAlignment();
+
+                //_sensor.Open();
+            }
+        }
+
+        private void BodyReader_FrameArrived(object sender, BodyFrameArrivedEventArgs e)
+        {
+            using (var frame = e.FrameReference.AcquireFrame())
+            {
+                if (frame != null)
+                {
+                    Body[] bodies = new Body[frame.BodyCount];
+                    frame.GetAndRefreshBodyData(bodies);
+
+                    Body body = bodies.Where(b => b.IsTracked).FirstOrDefault();
+
+                    if (!_faceSource.IsTrackingIdValid)
+                    {
+                        if (body != null)
+                        {
+                            _faceSource.TrackingId = body.TrackingId;
+                        }
+                    }
+                }
+            }
+        }
+
+        private void FaceReader_FrameArrived(object sender, HighDefinitionFaceFrameArrivedEventArgs e)
+        {
+            using (var frame = e.FrameReference.AcquireFrame())
+            {
+                if (frame != null && frame.IsFaceTracked)
+                {
+                    frame.GetAndRefreshFaceAlignmentResult(_faceAlignment);
+                    UpdateFacePoints();
+                }
+            }
+        }
+
+        private void UpdateFacePoints()
+        {
+            _points.Clear();
+            if (_faceModel == null) return;
+
+            var vertices = _faceModel.CalculateVerticesForAlignment(_faceAlignment);
+
+            if (vertices.Count > 0)
+            {
+                for (int index = 0; index < vertices.Count; index++)
+                {
+                    CameraSpacePoint vertice = vertices[index];
+                    ColorSpacePoint point = _sensor.CoordinateMapper.MapCameraPointToColorSpace(vertice);
+
+                    if (float.IsInfinity(point.X) || float.IsInfinity(point.Y))
+                        return;
+                    Vector2 _point = new Vector2(point.X, point.Y);
+                    _points.Add(_point);
+                }
+                points.OnNext(_points);
+            }
+        }
+
+        public void Dispose()
+        {
+            if (_faceModel != null)
+            {
+                _faceModel.Dispose();
+                _faceModel = null;
+            }
+
+            if (points != null)
+                points.Dispose();
+        }
+    }
+
+
+}
