788abfe698db03766a368b74e4983e9ad096b522
ravazquez <ravazquez@gmail.com>
2019-12-10 15:29:09 -0600

    Refactored FaceHD class

--- src/FaceHD.cs
+++ src/FaceHD.cs
@@ -4,10 +4,72 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reactive.Subjects;
+using VL.Lib.Collections;
 using Xenko.Core.Mathematics;
 
 namespace VL.Devices.Kinect2
 {
+    public class FaceHDData
+    {
+        private ColorSpacePoint[] _colorSpacePoints;
+
+        private DepthSpacePoint[] _depthSpacePoints;
+
+        public int TrackingId { get; }
+
+        public Quaternion FaceOrientation { get; }
+
+        public Vector3 HeadPivotPoint { get; }
+
+        public FaceAlignmentQuality FaceAlignmentQuality { get;  }
+
+        public IReadOnlyList<CameraSpacePoint> NativeCameraSpacePoints { get; }
+
+        public Spread<Vector2> RGBSpacePoints { 
+            get {
+                if (Sensor != null)
+                {
+                    if (_colorSpacePoints == null)
+                        _colorSpacePoints = new ColorSpacePoint[NativeCameraSpacePoints.Count];
+                    Sensor.CoordinateMapper.MapCameraPointsToColorSpace(NativeCameraSpacePoints.ToArray(), _colorSpacePoints);
+                }
+                return TypeConverter.ToVector2(_colorSpacePoints);
+            }  
+        }
+
+        public Spread<Vector2> DepthSpacePoints
+        {
+            get
+            {
+                if (Sensor != null)
+                {
+                    if (_depthSpacePoints == null)
+                        _depthSpacePoints = new DepthSpacePoint[NativeCameraSpacePoints.Count];
+                    Sensor.CoordinateMapper.MapCameraPointsToDepthSpace(NativeCameraSpacePoints.ToArray(), _depthSpacePoints);
+                }
+                return TypeConverter.ToVector2(_depthSpacePoints);
+            }
+        }
+
+        public Spread<Vector3> CameraSpacePoints
+        {
+            get
+            {
+                return TypeConverter.ToVector3(NativeCameraSpacePoints.ToArray());
+            }
+        }
+
+        public KinectSensor Sensor { get; }
+
+        public FaceHDData(IReadOnlyList<CameraSpacePoint> cameraSpacePoints, Microsoft.Kinect.Vector4 faceOrientation, CameraSpacePoint headPivotPoint, FaceAlignmentQuality quality, KinectSensor sensor)
+        {
+            NativeCameraSpacePoints = cameraSpacePoints;
+            FaceOrientation = TypeConverter.ToQuaternion(faceOrientation);
+            HeadPivotPoint = TypeConverter.ToVector3(headPivotPoint);
+            FaceAlignmentQuality = quality;
+            Sensor = sensor;
+        }
+    }
     public class FaceHD : IDisposable
     {
         private KinectSensor _sensor = null;
@@ -23,17 +85,17 @@ namespace VL.Devices.Kinect2
         private FaceAlignment _faceAlignment = null;
 
         private FaceModel _faceModel = null;
-        public IObservable<List<Vector2>> Points { get { return points; } }
 
-        private Subject<List<Vector2>> points;
+        private Subject<FaceHDData> faceHDData;
 
-        private List<Vector2> _points;
+        private FaceHDData _faceHDData;
+
+        public IObservable<FaceHDData> FaceHDData { get { return faceHDData; } }
 
         public FaceHD(KinectSensor sensor)
         {
             _sensor = sensor;
-            _points = new List<Vector2>();
-            points = new Subject<List<Vector2>>();
+            faceHDData = new Subject<FaceHDData>();
 
             if (_sensor != null)
             {
@@ -48,8 +110,6 @@ namespace VL.Devices.Kinect2
 
                 _faceModel = new FaceModel();
                 _faceAlignment = new FaceAlignment();
-
-                //_sensor.Open();
             }
         }
 
@@ -82,31 +142,20 @@ namespace VL.Devices.Kinect2
                 if (frame != null && frame.IsFaceTracked)
                 {
                     frame.GetAndRefreshFaceAlignmentResult(_faceAlignment);
-                    UpdateFacePoints();
+                    UpdateFaceData();
                 }
             }
         }
 
-        private void UpdateFacePoints()
+        private void UpdateFaceData()
         {
-            _points.Clear();
             if (_faceModel == null) return;
-
+            
             var vertices = _faceModel.CalculateVerticesForAlignment(_faceAlignment);
-
             if (vertices.Count > 0)
             {
-                for (int index = 0; index < vertices.Count; index++)
-                {
-                    CameraSpacePoint vertice = vertices[index];
-                    ColorSpacePoint point = _sensor.CoordinateMapper.MapCameraPointToColorSpace(vertice);
-
-                    if (float.IsInfinity(point.X) || float.IsInfinity(point.Y))
-                        return;
-                    Vector2 _point = new Vector2(point.X, point.Y);
-                    _points.Add(_point);
-                }
-                points.OnNext(_points);
+                _faceHDData = new FaceHDData(vertices, _faceAlignment.FaceOrientation, _faceAlignment.HeadPivotPoint, _faceAlignment.Quality, _sensor);
+                faceHDData.OnNext(_faceHDData);
             }
         }
 
@@ -117,9 +166,6 @@ namespace VL.Devices.Kinect2
                 _faceModel.Dispose();
                 _faceModel = null;
             }
-
-            if (points != null)
-                points.Dispose();
         }
     }
 
